05a5b16c7f072fceeceb908b2bc7f837cc87b617
Alexey Ignatov <alexey.ignatov.msu@gmail.com>
2020-11-30 23:57:13 +0300

    доп строка

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ def help(update, context):
 
 def echo(update, context):
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    update.message.reply_text(update.message.text+' доп строка')
 
 
 def error(update, context):
