f0beccb391a90297f96f0ec189684271c35f4a41
Alexey Ignatov <alexey.ignatov.msu@gmail.com>
2020-12-01 01:17:00 +0300

    обновили телефон

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ def help(update, context):
 
 def echo(update, context):
     """Echo the user message."""
-    update.message.reply_text(update.message.text+' доп строка ввв')
+    update.message.reply_text(update.message.text+' Наше телефон +7 953 000 00 00')
 
 
 def error(update, context):
